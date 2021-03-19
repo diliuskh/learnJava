1b920528028d7d748e450a9a09b28d64f474c165
diliuskh <diliuskh@gmail.com>
2021-03-19 16:44:42 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation("junit", "junit", "4.12")
+    testImplementation(platform("org.junit:junit-bom:5.7.1"))
+    testImplementation("org.junit.jupiter","junit-jupiter")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_15
 }
 tasks {
 
