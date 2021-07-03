b14fae9aabc3636949a9744c050cff2f2e8d9982
diliuskh <diliuskh@gmail.com>
2021-07-03 15:58:24 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,21 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion = "5.8.0-M1"
+val mockitoVersion = "3.11.2"
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.8.0-M1"))
-    testImplementation("org.junit.jupiter","junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter", "junit-jupiter")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params")
+    testImplementation("org.mockito", "mockito-core", mockitoVersion)
+    testImplementation("org.mockito", "mockito-junit-jupiter", mockitoVersion)
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_15
-    targetCompatibility = JavaVersion.VERSION_15
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
-tasks {
-
+tasks.test {
+    useJUnitPlatform()
 }
--- src/main/java/net/dilius/learnJava/Main.java
+++ src/main/java/net/dilius/learnJava/Main.java
@@ -3,6 +3,5 @@ package net.dilius.learnJava;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
